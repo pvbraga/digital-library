68233178ef0955384424a59696d7b280d2a9a3c8
Paulo Braga <pvbraga@me.com>
2016-06-22 22:07:08 -0300

    modify csv import

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
 	require 'csv'
 
   def self.import(file)
-    CSV.foreach(file.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
+    CSV.foreach(file.path, headers: true, encoding:'iso-8859-1') do |row|
 
       book_hash = row.to_hash # exclude the price field
       book = Book.where(id: book_hash["id"])
